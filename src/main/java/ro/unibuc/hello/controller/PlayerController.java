9f93c0da7737731886dd0b74751b4bd8df24a1ef
adrian <covaciadi77@gmail.com>
2023-06-13 01:21:05 +0300

    Delete player adaugat

--- src/main/java/ro/unibuc/hello/controller/PlayerController.java
+++ src/main/java/ro/unibuc/hello/controller/PlayerController.java
@@ -3,10 +3,7 @@ package ro.unibuc.hello.controller;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ro.unibuc.hello.data.PlayerEntity;
 import ro.unibuc.hello.data.PlayerRepository;
 
@@ -33,4 +30,11 @@ public class PlayerController {
     public PlayerEntity getPlayer(@RequestParam(name="id") String id) {
         return playerRepository.findById(String.valueOf(new ObjectId(id))).orElse(null);
     }
+
+    @DeleteMapping("/player/delete")
+    @ResponseBody
+    public void deletePlayer(@RequestParam(name="id") String id) {
+        playerRepository.deleteById(String.valueOf(new ObjectId(id)));
+    }
+
 }
